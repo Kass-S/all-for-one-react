a026a712a0bcb4f2f17f50d86143030dc6167b16
Kass-S <kassiespacek101@gmail.com>
2025-02-06 10:51:02 -0800

    madlib styling

--- src/components/MadLibComponent.jsx
+++ src/components/MadLibComponent.jsx
@@ -31,13 +31,11 @@ const MadLibComponent = () => {
 
             <Link to="/pages"><button className="text-black text-2xl m-5 bg-[#F0EBD9] rounded-xl border-[#DFD9C5] border-[3px] p-2 px-8">Back</button></Link>
 
-            <div className="text-black font-inder bg-white/70 grid mx-52 mt-14 grid-cols-5 grid-rows-6 rounded-3xl mr-[40rem] pb-5">
+
+            <div className="text-black font-inder bg-white/70 grid mx-52 mt-5 grid-cols-5 grid-rows-6 rounded-3xl mr-[40rem] pb-5">
 
                 <p className="text-3xl flex justify-center my-5 col-span-5">Mad Lib</p>
-
-                <p>{display}</p>
-
-                
+         
                 <input type="text" placeholder="Noun..." className="bg-white border-1 col-start-2 col-end-4 row-[2] m-3 rounded-xl mr-24" onChange={(event) => {
                     setMadLibNoun(event.target.value);
                 }} />
@@ -74,6 +72,8 @@ const MadLibComponent = () => {
                 <button className="text-2xl bg-white col-start-2 col-end-5 row-[6] rounded-xl border-[3px] border-black m-3 mx-28 cursor-pointer" onClick={displayMadLib}>Go!</button>
             
             </div>
+
+            <p className="bg-white/80 rounded-2xl mx-52 p-2 mt-2">{display}</p>
 
         </div>
     )

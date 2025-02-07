579e65cf1adc009bb938dbd7c72175089c1246bf
Kass-S <kassiespacek101@gmail.com>
2025-02-07 10:36:02 -0800

    say hello component responsiveness updated

--- src/components/SayHelloComponent.jsx
+++ src/components/SayHelloComponent.jsx
@@ -20,19 +20,18 @@ const SayHelloComponent = () => {
            
            <Link to="/pages"><button className="text-black text-2xl m-5 bg-[#F0EBD9] rounded-xl border-[#DFD9C5] border-[3px] p-2 px-8">Back</button></Link>
     
-            <div className="text-black font-inder bg-white/70 grid mx-52 mt-14 grid-cols-5 grid-rows-6 rounded-3xl">
-                <p className="text-black text-3xl flex justify-center my-5 col-[3]">Say Hello</p>
+            <div className="text-black font-inder bg-white/70 grid mx-8  lg:mx-52 mt-10 grid-cols-1 grid-rows-4 lg:grid-cols-5 lg:grid-rows-6 rounded-3xl">
 
-                
+                <p className="text-black text-3xl flex justify-center my-5 col-[1] lg:col-start-2 lg:col-end-5">Say Hello</p>
 
-                <input type="text" placeholder="Name here..." className="bg-white border-1 border-black col-[3] row-[3] m-3 rounded-xl" onChange={(event) => { 
+                <input type="text" placeholder="Name here..." className="bg-white border-1 border-black my-5 col-[1] lg:col-[3] row-[3] m-3 mx-20 md:mx-40 lg:mx-0 rounded-xl" onChange={(event) => { 
                         fetchGreeting(event.target.value);
                     }
                 } />
 
-                <button className="text-2xl bg-white border-[3px] border-black col-[3] row-[4] m-3 rounded-xl cursor-pointer" onClick={displayGreeting}>Go!</button>
+                <button className="text-2xl bg-white border-[3px] border-black my-5 col-[1] lg:col-[3] row-[4] m-3 mx-20 md:mx-40 lg:mx-0 rounded-xl cursor-pointer" onClick={displayGreeting}>Go!</button>
 
-                <p className="flex justify-center self-center col-start-[2] col-end-[5] row-[2] ">{display}</p>
+                <p className="flex justify-center self-center lg:col-start-[2] lg:col-end-[5] row-[2] ">{display}</p>
                 
             </div>
         </div>
